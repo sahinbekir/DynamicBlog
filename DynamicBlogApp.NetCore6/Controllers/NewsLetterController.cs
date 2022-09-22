ee35f899d4e3dfb3442c2ec333ccb2136bb1c0d1
schahinbekir <bsbekirsahinbs@gmail.com>
2022-09-22 13:58:26 +0300

    gmail subscribe.

---
+++ DynamicBlogApp.NetCore6/Controllers/NewsLetterController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynamicBlogApp.NetCore6.Controllers
+{
+	public class NewsLetterController : Controller
+	{
+		NewsLetterManager nm = new NewsLetterManager(new EfNewsLetterRepository());
+		[HttpGet]
+		public PartialViewResult SubsMail()
+		{
+			return PartialView();
+		}
+		[HttpPost]
+        public PartialViewResult SubsMail(NewsLetter p)
+        {
+			p.MailStatus = true;
+			nm.AddNewsLetter(p);
+            return PartialView();
+        }
+    }
+}
